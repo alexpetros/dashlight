b1f3f2c0e144c734aa6af8c60ff34b33c71ed171
Alexander Petros <apetros15@gmail.com>
2022-05-20 23:16:07 -0400

    Add argument parser tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,30 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::io;
-
+use std::io::{BufRead, BufReader};
 
 use crate::parsers::nginx;
 mod parsers;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidArgs,
     ParsingError,
 }
 
+#[derive(Debug)]
 pub struct Config {
     pub filename: Option<String>,
 }
 
 impl Config {
+    // TODO: convert to OsString
     pub fn new(args: &[String]) -> Result<Config, Error> {
         if args.len() == 1 {
-           return Ok(Config { filename: None });
-        } else if args.len() == 2{
-            return Ok(Config { filename: Some(args[1].clone()) });
+            return Ok(Config { filename: None });
+        } else if args.len() == 2 {
+            return Ok(Config {
+                filename: Some(args[1].to_string()),
+            });
         }
 
         Err(Error::InvalidArgs)
@@ -36,7 +38,7 @@ pub fn run(config: Config) -> Result<(), io::Error> {
             let file = File::open(filename)?;
             Box::new(BufReader::new(file))
         }
-        None => Box::new(BufReader::new(io::stdin()))
+        None => Box::new(BufReader::new(io::stdin())),
     };
 
     let mut x2: u32 = 0;
@@ -49,10 +51,10 @@ pub fn run(config: Config) -> Result<(), io::Error> {
         let log = nginx::get_log_from_logline(&logline).unwrap();
 
         match log.status {
-            200..=299 => { x2 += 1 }
-            300..=399 => { x3 += 1 }
-            400..=499 => { x4 += 1 }
-            500..=599 => { x5 += 1 }
+            200..=299 => x2 += 1,
+            300..=399 => x3 += 1,
+            400..=499 => x4 += 1,
+            500..=599 => x5 += 1,
             _ => {}
         }
     }
@@ -65,3 +67,28 @@ pub fn run(config: Config) -> Result<(), io::Error> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invalid_args_missing_name() {
+        let args: Vec<String> = Vec::new();
+        let config_result = Config::new(&args).unwrap_err();
+        assert_eq!(config_result, Error::InvalidArgs);
+    }
+
+    #[test]
+    fn no_args_no_filename() {
+        let args = ["dashlight".to_string()];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.filename, None);
+    }
+
+    #[test]
+    fn one_arg_filename() {
+        let args = ["dashlight".to_string(), "access.log".into()];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.filename, Some("access.log".to_string()));
+    }
+}
